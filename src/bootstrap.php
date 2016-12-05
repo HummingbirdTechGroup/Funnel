d9401213a0c73f1e09782f4c711d98d09db94b1c
Carlos Ortega <carlosV2.0@gmail.com>
2016-12-05 09:59:19 +0000

    Add missing filters

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+use carlosV2\Funnel\Filter\AllOfFilter;
+use carlosV2\Funnel\Filter\AnyOfFilter;
+use carlosV2\Funnel\Filter\BeingFilter;
+use carlosV2\Funnel\Filter\HavingFilter;
 use \carlosV2\Funnel\Funnel;
 use \carlosV2\Funnel\Filter\MethodFilter;
 use \carlosV2\Funnel\Filter\MethodsFilter;
@@ -9,6 +13,10 @@ use \carlosV2\Funnel\Filter\TypeFilter;
 use carlosV2\Funnel\Repository\FrozenRepository;
 
 // Register filters
+Funnel::addFilter(new AllOfFilter());
+Funnel::addFilter(new AnyOfFilter());
+Funnel::addFilter(new BeingFilter());
+Funnel::addFilter(new HavingFilter());
 Funnel::addFilter(new MethodFilter());
 Funnel::addFilter(new MethodsFilter());
 Funnel::addFilter(new PropertyFilter());
